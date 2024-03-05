b16963b422bc1ef92b4cabdc578593e0cf5e5387
Abdulajishs <abdulajeeshsb@gmail.com>
2024-03-05 08:47:28 +0530

    Auto logout

--- src/store/tokenContextProvider.js
+++ src/store/tokenContextProvider.js
@@ -1,21 +1,32 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import TokenContext from "./tokenContext"
 
 const TokenContextProvider = (props) => {
     const initialToken = localStorage.getItem("token");
     const [enteredTokens, setEnteredTokens] = useState(initialToken)
 
+    
     const userIsLoggedIn = !!enteredTokens;
-
+    
     const addTokenHandler = (token) => {
         setEnteredTokens(token)
-        localStorage.setItem("token", enteredTokens)
+        localStorage.setItem("token", token)
     }
-
-    const removeTokenHandler = () => {
+    
+    const removeTokenHandler = useCallback(() => {
         setEnteredTokens(null)
         localStorage.removeItem("token")
-    }
+    },[])
+    
+    useEffect(()=>{
+        let timer;
+        if (userIsLoggedIn) {
+            timer  = setTimeout(() => {
+                removeTokenHandler()
+            }, 300000);
+        }
+        return () => clearTimeout(timer)
+    },[userIsLoggedIn,removeTokenHandler])
 
     const tokenContext = {
         token: enteredTokens,
